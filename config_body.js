812250c77ab2a4f6dc722208201dedf651b8c84c
Bainsetnature <79660829+Bainsetnature@users.noreply.github.com>
2022-12-12 14:04:40 +0100

    Update config_body.js

--- config_body.js
+++ config_body.js
@@ -42,7 +42,7 @@ var Promotions = [
     {
         title: "Soins Corps",
         buttonText: "Cliquez ici",
-        link: '',
+        link: 'transactions/scope_items/0266dd41308f4d26bad9cc7e36fe075f?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%225332e389-e1f9-421e-bc33-689bdfbca16c%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%2299207e51-47b5-4f30-8271-c364a6882050%5C%5C%5C%22%7D%5C%22%7D%22&StartIndex=0&SearchString=&SearchAll=false',
         image: 'https://bainsetnature.github.io/SitePepperi/COFFRET MOANA.png'
     }
     ]
@@ -50,7 +50,7 @@ var Promotions2 = [
     {
         title: "Parfums Maison",
         buttonText: "Cliquez ici",
-        link: '',
+        link: 'transactions/scope_items/0266dd41308f4d26bad9cc7e36fe075f?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%2218d7976d-11b2-44ea-9a4f-c3c649e98ad1%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%2229b100e3-4dfa-4ec8-98fd-e93e8e762939%5C%5C%5C%22%7D%5C%22%7D%22&StartIndex=0&SearchString=&SearchAll=false',
         image: 'https://bainsetnature.github.io/SitePepperi/delirium floral home.png'
     }
     ]
@@ -58,7 +58,7 @@ var Promotions3 = [
     {
         title: "Les Nomades",
         buttonText: "Cliquez ici",
-        link: '',
+        link: 'transactions/scope_items/0266dd41308f4d26bad9cc7e36fe075f?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%225332e389-e1f9-421e-bc33-689bdfbca16c%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%2209fd1d0b-e9f8-414f-9c10-e3e4b72049ee%5C%5C%5C%22%7D%5C%22%7D%22&StartIndex=0&SearchString=&SearchAll=false',
         image: 'https://bainsetnature.github.io/SitePepperi/Présentoir eau parfums.png'
     }
     ]
@@ -66,7 +66,7 @@ var Promotions4 = [
     {
         title: "Offres Cadeaux",
         buttonText: "Cliquez ici",
-        link: '',
+        link: 'Transactions/scope_items/01d112a871b24497bf47210acc18eca2?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22d332767e-acf9-42db-bbf8-fcd15ec1b0b3%5C%22%7D%22&StartIndex=0&SearchString=&SearchAll=false',
         image: 'https://bainsetnature.github.io/SitePepperi/P1140485-LIGHT.png'
     }
 ]
