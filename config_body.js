07690d3ede5a0229c943dfacbcda4acc3b70b23a
Bainsetnature <79660829+Bainsetnature@users.noreply.github.com>
2023-05-04 09:13:48 +0200

    Update config_body.js

--- config_body.js
+++ config_body.js
@@ -54,7 +54,7 @@ var Promotions2 = [
     {
         title: "Parfums Maison",
         buttonText: "Cliquez ici",
-        link: 'transactions/scope_items/0266dd41308f4d26bad9cc7e36fe075f?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%2218d7976d-11b2-44ea-9a4f-c3c649e98ad1%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%2229b100e3-4dfa-4ec8-98fd-e93e8e762939%5C%5C%5C%22%7D%5C%22%7D%22&StartIndex=0&SearchString=&SearchAll=false',
+        link: 'transactions/scope_items/0266dd41308f4d26bad9cc7e36fe075f?CurrentTab="%7B%5C"JsonFilter%5C":%5C"18d7976d-11b2-44ea-9a4f-c3c649e98ad1%5C",%5C"Parent%5C":%5C"%7B%5C%5C%5C"JsonFilter%5C%5C%5C":%5C%5C%5C"99207e51-47b5-4f30-8271-c364a6882050%5C%5C%5C"%7D%5C"%7D"&StartIndex=0&SearchString=&SearchAll=false',
         image: 'https://bainsetnature.github.io/SitePepperi/delirium floral home.png'
     }
     ]
